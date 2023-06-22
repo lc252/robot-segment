207c012dd6f7932d7e088bfc30cd6fb71d8bf317
lc252 <lc252@uowmail.edu.au>
2023-06-22 20:16:01 +1000

    added blur and modularised

--- python_ws/robot_augment_label.py
+++ python_ws/robot_augment_label.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 
-# combination of robot_label and impose_robot_background
-
-
-
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageFilter
 from pathlib import Path
 from scipy.ndimage import binary_closing
 from rasterio import features
@@ -16,6 +12,7 @@ import random
 
 
 def get_mask(rob):
+    rob = np.array(rob)
     rob_mask = np.zeros(rob.shape[0:2])
     for row in range(480):
         for col in range(640):
@@ -23,57 +20,74 @@ def get_mask(rob):
                 rob_mask[row][col] = 1
     return rob_mask
 
-def impose_background(rob, rob_mask, background_path, rob_path):
+def impose_background(rob, rob_mask, background_path):
     # load
-    im = Image.open(background_path)
-    im_arr = np.array(im.resize((640,480)))
+    bkgd = np.array(Image.open(background_path).resize((640,480)))
+    rob = np.array(rob)
 
     # put rob on background according to mask
     for row in range(480):
         for col in range(640):
             if rob_mask[row][col]:
                 # rob is rgba
-                im_arr[row][col] = rob[row][col][0:3]
-    # render
-    # print("/home/fif/lc252/datasets" + str(rob_path)[28:])
-    plt.imsave("/home/fif/lc252/datasets" + str(rob_path)[28:], im_arr)
-    return im_arr
+                bkgd[row][col] = rob[row][col][0:3]
+    
+    # # Debug Display
+    # plt.imshow(Image.fromarray(bkgd))
+    # plt.show()
 
+    return Image.fromarray(bkgd)
 
-def create_label(rob, rob_mask, label_path):
-    arr = rob # np.asarray(Image.open(image_path))
-
+def create_label(rob_mask, label_path : Path):
     rob_mask = binary_closing(rob_mask, iterations=2).astype(np.int16)
 
+    # fit bounding polygon to mask, return list of coordinates
     cords = list(features.shapes(rob_mask, mask=(rob_mask > 0), connectivity=8))[0][0]['coordinates'][0]
 
+    # # Display for debug
     # poly = Polygon(cords)
     # x,y = poly.exterior.xy
     # plt.imshow(rob)
     # plt.plot(x,y)
     # plt.show()
 
-    label_line = '0 ' + ' '.join([f'{int(cord[0])/arr.shape[1]} {int(cord[1])/arr.shape[0]}' for cord in cords])
+    # create yolo-compatible annotation of bounding polygon coordinates in format: "[class id] [x1 y1 x2 y2 ...]" where x and y are normalised (0-1)
+    # The first "0" is the class label, here it is hardcoded as there is only one class
+    label_line = '0 ' + ' '.join([f'{int(cord[0])/rob_mask.shape[1]} {int(cord[1])/rob_mask.shape[0]}' for cord in cords])
 
+    # write annotation file
     label_path.parent.mkdir( parents=True, exist_ok=True )
     with label_path.open('w') as f:
         f.write(label_line)
 
-if __name__ == "__main__":
-    
-    background_dir = list(Path("/home/fif/lc252/background_images").iterdir())
 
-    for images_dir_path in [Path(f'/home/fif/lc252/datasets_raw/{x}/images') for x in ['train', 'val', 'test']]:
-        for img_path in images_dir_path.iterdir():
-            # load robot with green and do random rotate
-            rob = np.asarray(Image.open(img_path).rotate(random.randint(-30,30)))
+
+if __name__ == "__main__":
+
+    PATH = "/home/lachl/images"
+    
+    background_dir = list(Path(f"{PATH}/background_images").iterdir())
+
+    # iterate each image in train, val, test sets
+    for sets in ['train', 'val', 'test']:
+        images_dir = Path(f"{PATH}/datasets_mask/{sets}/images")
+        for img_path in images_dir.iterdir():
+            # load robot with green background
+            rob = Image.open(img_path)
+            # rotate robot randomly
+            rob = rob.rotate(random.randint(-30,30))
             # get mask
             rob_mask = get_mask(rob)
             # put robot on random background
-            rob = impose_background(rob, rob_mask, random.choice(background_dir), img_path)
-            # get the label path
-            label_path = images_dir_path.parent.parent.parent / "datasets" / f"{str(images_dir_path)[29:-7]}" / "labels" / f"{img_path.stem}.txt"
+            rob = impose_background(rob, rob_mask, random.choice(background_dir))
+            # apply gaussian blur to im_arr
+            rob = rob.filter(ImageFilter.GaussianBlur(radius=1))
+            # save image
+            # plt.imshow(rob)
+            # plt.show()
+            plt.imsave(f"{PATH}/datasets/{sets}/images/{img_path.stem}.png", np.array(rob))
+            # save annotation
+            label_path = Path(f"{PATH}/datasets/{sets}/labels/{img_path.stem}.txt")
+            create_label(rob_mask, label_path)
 
-            create_label(rob, rob_mask, label_path)
-
-            print(img_path, label_path)
+            print("Completed:", img_path, label_path)
